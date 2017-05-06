ea2e71e2d3fb4a937e517dbb2fd459105878a485
cmckillop <conor303@gmail.com>
2017-05-06 14:32:05 +0100

    Remove Placeholder Code
    Remove Xcode placeholder code from project files.

--- Squared/GameViewController.swift
+++ Squared/GameViewController.swift
@@ -17,27 +17,20 @@ class GameViewController: UIViewController {
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let scene = GameScene(fileNamed: "GameScene") {
+                
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+            // Present the scene
+            if let view = self.view as! SKView? {
+                view.presentScene(scene)
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+                view.ignoresSiblingOrder = true
                 
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
+                view.showsFPS = true
+                view.showsNodeCount = true
+                view.showsPhysics = true
             }
         }
     }
